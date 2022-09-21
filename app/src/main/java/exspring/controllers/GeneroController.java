4ed8175d536fccca3759584c697ed38ae34de31c
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2022-09-21 17:56:51 +0000

    exspring

--- app/src/main/java/exspring/controllers/GeneroController.java
+++ app/src/main/java/exspring/controllers/GeneroController.java
@@ -3,19 +3,24 @@ package exspring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model; //u-user;i-interface; -> usada quando quero passar uma informação para o jsp
 
 import exspring.repositories.GeneroRepository;
 
 @Controller
 @RequestMapping("/generos")
-public class GeneroControler{
+public class GeneroController{
     @Autowired
     private GeneroRepository generosRepo;
 
     @RequestMapping("list")
     public String list(Model model) {
-        model.addAtribute("generos", this.generosRepo.findAll());
+        model.addAttribute("generos", this.generosRepo.findAll());
         return "list";
+    }
+
+    @RequestMapping("insert")
+    public String insert() {
+        return "insert";
     }
 }
